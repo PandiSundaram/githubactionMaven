1170ec37d615aec619c463b3e33c91c3f777c294
pandisundaram2812 <pandi.sundaram@anywhere.co>
2023-09-22 12:53:02 +0530

    first commit

---
+++ src/main/java/com/pandi/practice/githubmaven/GithubmavenApplication.java
@@ -0,0 +1,13 @@
+package com.pandi.practice.githubmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubmavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubmavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pandi/practice/githubmaven/GithubmavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandi.practice.githubmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubmavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
